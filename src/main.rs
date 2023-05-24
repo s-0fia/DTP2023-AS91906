e23eb0606956619efa955fb11f4693567196bc9d
Elle Fleming <39723774+s-0fia@users.noreply.github.com>
2023-05-24 21:17:54 +1200

    Added public and static serving
    + Added public directory for publicly accessible files such as html files
    + Added static directory for static files such as javascript and css files
    - Removed default basic hard coded html serve
    + Added basic index.html which is served as "/"
    + Made all public routes without an extension assume html extension and search for that path + .html in directory

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{routing::get, Router};
 use itertools::Itertools;
 use std::net::SocketAddr;
 mod shutdown;
+mod handlers;
 
 // Initialise const values for server stuff.
 const SERV_PORT: u16 = 80;
@@ -9,19 +10,19 @@ const IP_ADDR: [u8; 4] = [127, 0, 0, 1];
 
 #[tokio::main]
 async fn main() {
-    // Build the app with the '/' route forwarding to the handler() function.
-    let app = Router::new().route("/", get(handler));
+    // Build the routes for the app, handling the static and public directories with an exception for / going to "index.html".
+    let app = Router::new()
+        .route("/", get(handlers::home_path))
+        .route("/*path", get(handlers::public_path))
+        .route("/static/*path", get(handlers::static_path));
 
-    // Print the ip and port the server is hosted on, then bind and serve the server.
-    println!("listening on {}:{}", IP_ADDR.iter().format("."), SERV_PORT);
+    // Print the ip and port the server is hosted on.
+    println!("Server hosted statically on {}:{}", IP_ADDR.iter().format("."), SERV_PORT);
+
+    // Bind the server with the routes and have a graceful shutdown signal.
     axum::Server::bind(&SocketAddr::from((IP_ADDR, SERV_PORT)))
         .serve(app.into_make_service())
         .with_graceful_shutdown(shutdown::shutdown_signal())
         .await
         .unwrap();
-}
-
-// A simple rHTML function that shows as a default page.
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
 }
